77955ab7d3abde1a50acd8a6c3462ff43bc4f4ca
thennek0 <kennethbecaro0@gmail.com>
2023-06-22 21:11:01 +0800

    setup initla files

---
+++ src/components/functionality/gameboard.js
@@ -0,0 +1,6 @@
+// gameboard that facilitates the game loop
+// store scores in gameboard
+// keep track of already clicked cards
+// each time an unclicked card is click increase score
+// when card clicked is already clicked reset score and check if current score is high
+// have function that shuffles card positions
